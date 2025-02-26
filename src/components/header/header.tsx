139e4a452428e1b20bc73baf028b289bdad41b4d
daeun <qzsz797979@gmail.com>
2025-02-26 11:36:40 +0900

    design: Header 기본 UI 구현

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -2,22 +2,32 @@
 
 import { useAuthContext } from '@/contexts/auth-context';
 import Image from 'next/image';
+import Link from 'next/link';
 
 export default function Header() {
   const { user, login, logout } = useAuthContext();
 
   return (
-    <header>
-      {user && (
-        <div>
-          {user.photoURL && (
-            <Image src={user.photoURL} width={30} height={30} alt="프로필 이미지" />
-          )}
-          <p>{user.displayName}</p>
-        </div>
-      )}
-      {user && <button onClick={logout}>로그아웃</button>}
-      {user === null && <button onClick={login}>로그인</button>}
+    <header className="flex items-center justify-between p-2 border-b ">
+      <Link href="/">Oh Planner</Link>
+      <div className="flex items-center gap-3">
+        {user && (
+          <div className="flex items-center gap-2">
+            {user.photoURL && (
+              <Image
+                src={user.photoURL}
+                width={30}
+                height={30}
+                alt="프로필 이미지"
+                className="rounded-full shadow-sm"
+              />
+            )}
+            <p>{user.displayName}</p>
+          </div>
+        )}
+        {user && <button onClick={logout}>로그아웃</button>}
+        {user === null && <button onClick={login}>로그인</button>}
+      </div>
     </header>
   );
 }
